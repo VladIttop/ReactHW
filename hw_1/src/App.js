8991c182b1518dafff8559458b586bc448bbbca9
VladIttop <129985238+VladIttop@users.noreply.github.com>
2025-03-08 20:07:56 +0000

    add hw_1

---
+++ hw_1/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Task from "./Task";
+
+function App() {
+  const text = "World";
+  return (
+    <>
+    <h1>Hello, {text}!</h1>
+      <Task />
+    </>
+  );
+}
+
+export default App;
